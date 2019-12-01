a6c72afb51e9c9b024cdf507f471e635bcad371f
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-01 18:14:40 +0000

    feat(renderer): add way to delete item

--- renderer/items.js
+++ renderer/items.js
@@ -9,6 +9,49 @@ fs.readFile(`${__dirname}/reader.js`, (err, data) => {
 // track items in storage
 exports.storage = JSON.parse(localStorage.getItem("readit-items")) || [];
 
+window.addEventListener("message", event => {
+  if (event.data.action === "delete-reader-item") {
+    event.source.close();
+    this.delete(event.data.itemIndex);
+    // close the window
+  }
+});
+
+exports.delete = itemIndex => {
+  console.log(itemIndex)
+  // remove item from DOM
+  items.removeChild(items.childNodes[itemIndex]);
+
+  // remove from storage
+  this.storage.splice(itemIndex, 1);
+
+  // persist new storage
+  this.save();
+
+  // select prev item or select new item now first if present
+  if (this.storage.length) {
+    let newSelectedItemIndex = itemIndex === 0 ? 0 : itemIndex - 1;
+
+    // set item at new index as selected
+    document
+      .getElementsByClassName("read-item")
+      [newSelectedItemIndex].classList.add("selected");
+  }
+};
+
+// get index of an item
+exports.getSelectedItem = () => {
+  // get node
+  let currentItem = document.getElementsByClassName("read-item selected")[0];
+
+  // get item index
+  let itemIndex = 0;
+  let child = currentItem;
+  while ((child = child.previousSibling) != null) itemIndex++;
+
+  return { node: currentItem, index: itemIndex };
+};
+
 exports.save = () => {
   localStorage.setItem("readit-items", JSON.stringify(this.storage));
 };
@@ -16,16 +59,14 @@ exports.save = () => {
 // set item as selected
 exports.select = event => {
   // remove currently selected item
-  document
-    .getElementsByClassName("read-item selected")[0]
-    .classList.remove("selected");
+  this.getSelectedItem().node.classList.remove("selected");
 
   // add to clicked item
   event.currentTarget.classList.add("selected");
 };
 
 exports.changeSelection = direction => {
-  let currentItem = document.getElementsByClassName("read-item selected")[0];
+  let currentItem = this.getSelectedItem().node;
 
   if (
     direction === "ArrowUp" &&
@@ -45,10 +86,10 @@ exports.open = () => {
   if (!this.storage.length) return;
 
   // get the selected item
-  let selectedItem = document.getElementsByClassName("read-item selected")[0];
+  let selectedItem = this.getSelectedItem();
 
   // get the url
-  let contentURL = selectedItem.dataset.url;
+  let contentURL = selectedItem.node.dataset.url;
 
   // open item in proxy BrowserWindow - nodeIntegration assumes true - uh oh
   // also has same size limits as main window
@@ -67,7 +108,7 @@ exports.open = () => {
   );
 
   // inject javascript
-  readerWin.eval(readerJS);
+  readerWin.eval(readerJS.replace("{{index}}", selectedItem.index));
 };
 
 exports.addItem = (item, isNew = false) => {
